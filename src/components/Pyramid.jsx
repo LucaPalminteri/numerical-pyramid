d6950d752bf427f2e2947c37d65776d25bfa2aa2
LucaPalminteri <lucapalminteri02@gmail.com>
2022-04-21 11:03:56 -0300

    first pyramid model OK

--- src/components/Pyramid.jsx
+++ src/components/Pyramid.jsx
@@ -43,13 +43,18 @@ export default function Pyramid() {
 
     function newPyramid(arr) {
 
+        for (let i = 0; i < arr.length; i++) {
+            for (let j = 0; j < arr[i].length; j++) {
+            }
+        }
+
         return(
             <div>
                 <div><input onChange={changeInput} value={arr[4]}/></div>
                 <div><input onChange={changeInput} value={arr[3][0]}/><input onChange={changeInput} value={arr[3][1]}/> </div>
-                <div>{arr[2][0]} - {arr[2][1]} - {arr[2][2]}</div>
-                <div>{arr[1][0]} - {arr[1][1]} - {arr[1][2]} - {arr[1][3]}</div>
-                <div>{arr[0][0]} - {arr[0][1]} - {arr[0][2]} - {arr[0][3]} - {arr[0][4]}</div>
+                <div><input onChange={changeInput} value={arr[2][0]}/><input onChange={changeInput} value={arr[2][1]}/><input onChange={changeInput} value={arr[2][2]}/></div>
+                <div><input onChange={changeInput} value={arr[1][0]}/><input onChange={changeInput} value={arr[1][1]}/><input onChange={changeInput} value={arr[1][2]}/><input onChange={changeInput} value={arr[1][3]}/> </div>
+                <div><input onChange={changeInput} value={arr[0][0]}/><input onChange={changeInput} value={arr[0][1]}/><input onChange={changeInput} value={arr[0][2]}/><input onChange={changeInput} value={arr[0][3]}/><input onChange={changeInput} value={arr[0][4]}/> </div>
             </div>
         )
     }
