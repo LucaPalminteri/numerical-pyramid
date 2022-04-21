0640f724c9edc062f217bc43fdb931ee79c0a324
LucaPalminteri <lucapalminteri02@gmail.com>
2022-04-21 10:58:43 -0300

    remove fake pyramid

--- src/components/Pyramid.jsx
+++ src/components/Pyramid.jsx
@@ -1,44 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { nanoid } from "nanoid";
 
 export default function Pyramid() {
     const test = createBase(5)
-    
-    function createLine(n) {
-        const line = [];
-        for (let i = 0; i < n; i++) {
-            line.push(
-                <input 
-                    key={nanoid()} 
-                    type='number' 
-                    min={0}
-                    value={Math.floor(Math.random()*100)}
-                    onChange={changeInput}/>
-            )
-        }
-        line.push(<br key={nanoid()}/>)
-        return(line)
-    } 
+    ordenatedBase(test)
 
-    function createPyramid(arr) {
-        let n = arr.length
-        const pyramid = []
-        for (let i = n; i > 0; i--) {
-            pyramid.push(createLine(n))
-            n--;
-        }
-        return (pyramid.reverse())
-    }
+    useEffect(()=> {
+
+    },[])
 
     function changeInput() {
 
-    }
-
-    function randomBase(n) {
-        const arr = []
-        for (let i = 0; i < n; i++) {
-            arr.push(Math.floor(Math.random()*5))
-        }
     }
 
     function createBase(n) {
@@ -46,7 +18,7 @@ export default function Pyramid() {
         for (let i = n; i > 0; i--) {
             const arr = []
                 for (let i = 0; i < n; i++) {
-                    arr.push(Math.floor(Math.random()*5))
+                    arr.push(Math.ceil(Math.random()*9))
                 }
             base.push(arr)
             n--;
@@ -59,11 +31,9 @@ export default function Pyramid() {
            for (let j = 0; j < arr[i].length; j++) {
                if(i > 0) {
                 arr[i][j] = arr[i-1][j] + arr[i-1][j+1]
-                //console.log(`${i}-${j}: ${arr[i-1][j] + arr[i-1][j+1]} (${arr[i-1][j]} + ${arr[i-1][j+1]})`);
                }
                else if (i === 0) {
                 arr[i][j] = arr[i][j]
-                //console.log(`${i}-${j}: ${arr[i][j]}`);
                }
            }
         }
@@ -71,14 +41,24 @@ export default function Pyramid() {
         return(arr);
     }
 
-    ordenatedBase(test)
+    function newPyramid(arr) {
 
-    console.log(test)
+        return(
+            <div>
+                <div><input onChange={changeInput} value={arr[4]}/></div>
+                <div><input onChange={changeInput} value={arr[3][0]}/><input onChange={changeInput} value={arr[3][1]}/> </div>
+                <div>{arr[2][0]} - {arr[2][1]} - {arr[2][2]}</div>
+                <div>{arr[1][0]} - {arr[1][1]} - {arr[1][2]} - {arr[1][3]}</div>
+                <div>{arr[0][0]} - {arr[0][1]} - {arr[0][2]} - {arr[0][3]} - {arr[0][4]}</div>
+            </div>
+        )
+    }
+
+    //console.log(test.reverse())
 
     return (
         <div>
-            {createPyramid(test)}
-
+            {newPyramid(test)}
         </div>
     )
 }
